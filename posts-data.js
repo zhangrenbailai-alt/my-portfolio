e62a406887193dbf8385bed1ad3e75df2c72efd9
zhangrenbailai-alt <zhangrenbailai@gmail.com>
2026-06-19 11:53:55 +0900

    Add files via upload

--- posts-data.js
+++ posts-data.js
@@ -1,4 +1,37 @@
 const POSTS = [
+
+  { 
+    id: "20260618-post2",       // ファイル名から.htmlを除いたもの
+    tag: "calligraphy",               // タグ名
+    title: "北斎まんが風QRコードの制作",  // 記事タイトル
+    date: "2026.06.18",          // 日付
+    url: "posts/20260618-post2.html"  // ファイルのパス
+  },
+
+  { 
+    id: "20260618-post1",       // ファイル名から.htmlを除いたもの
+    tag: "calligraphy",               // タグ名
+    title: "ひらがな風QRコードの制作",  // 記事タイトル
+    date: "2026.06.18",          // 日付
+    url: "posts/20260618-post1.html"  // ファイルのパス
+  },
+
+　{ 
+    id: "20260617-post1",       // ファイル名から.htmlを除いたもの
+    tag: "calligraphy",               // タグ名
+    title: "漢字風QRコードの制作",  // 記事タイトル
+    date: "2026.06.17",          // 日付
+    url: "posts/20260617-post1.html"  // ファイルのパス
+  },
+
+ { 
+    id: "20260616-post2",       // ファイル名から.htmlを除いたもの
+    tag: "calligraphy",               // タグ名
+    title: "どうやって読み込み可能なQRコード書道を作るか",  // 記事タイトル
+    date: "2026.06.16",          // 日付
+    url: "posts/20260616-post2.html"  // ファイルのパス
+  },
+
  { 
     id: "20260616-post1",       // ファイル名から.htmlを除いたもの
     tag: "calligraphy",               // タグ名
